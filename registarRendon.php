c00dc9294662b646b6a48c92501701465b98e4ff
ivan27032000 <ivanrendon00@gmail.com>
2022-02-26 17:52:49 -0500

    RegistroRendon terminado

---
+++ registarRendon.php
@@ -0,0 +1,42 @@
+<?php
+    //include('conexionBeltran.php');
+
+    //Base de datos Rendon con contraseña
+    //
+    $conex = mysqli_connect("localhost", "root", "admin", "grupo2");
+
+    if(isset($_POST['nombre']) &&
+        isset($_POST['apellidos']) &&
+        isset($_POST['correo']) &&
+        isset($_POST['asunto']) &&
+        isset($_POST['mensaje'])
+    ){
+
+        $telefono;
+
+        if(isset($_POST['telefono'])){
+            $telefono = $_POST['telefono'];
+        }else{
+            $telefono = '';
+        }
+
+        $nombre = $_POST['nombre'];
+        $apellidos = $_POST['apellidos'];
+        $correo = $_POST['correo'];
+        $asunto = $_POST['asunto'];
+        $mensaje = $_POST['mensaje'];
+
+        $sql = "INSERT INTO mas_informacion (`nombre`, `apellidos`, `telefono`, `correo`, `asunto`, `mensaje`) 
+                VALUES ('$nombre', '$apellidos', '$telefono', '$correo', '$asunto', '$mensaje');";
+
+        $resultado = mysqli_query($conex, $sql);
+
+        if($resultado){
+            echo "<h2>Mensaje enviado correctamente</h2>";
+
+            header('Location: FormularioRendónIván.html');
+        }
+
+    }
+
+?>
