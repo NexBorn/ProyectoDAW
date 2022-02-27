9525c52e17362641a7f16ec1b373c23b0f3d6f13
ivan27032000 <ivanrendon00@gmail.com>
2022-02-27 13:18:00 -0500

    EliminarRendon.php Terminado

--- navRendon.php
+++ navRendon.php
@@ -1,12 +1,10 @@
-
-
-<nav>
-    <div class="nav-links" style=" position: relative; top: -10rem;">
+<nav style=" position: absolute; top: -1rem;">
+    <div class="nav-links">
         <ul>
             <li><a href="index.html">Inicio</a></li>
             <li><a href="FormularioRendónIván.php">Añadir</a></li>
             <li><a href="listarRendon.php">Listar</a></li>
-            <li><a href="">Actulizar</a></li>
+            <li><a href="editarRendon.php">Actulizar</a></li>
             <li><a href="eliminarRendon.php">Eliminar</a></li>
         </ul>
     </div>
