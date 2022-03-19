23411435ad0529c847f22e6cef3fc5e7ff14a99a
ivan27032000 <ivanrendon00@gmail.com>
2022-03-19 15:02:08 -0500

    Rendon DAO Terminado

--- modelo/dao/RendonDAO.php
+++ modelo/dao/RendonDAO.php
@@ -10,10 +10,10 @@ class ProductosDAO {
         $this->con = Conexion::getConexion();
     }
 
-    
+    //Terminado
     public function listar() {
         // sql de la sentencia
-        $sql = "select * from productos, categoria where prod_idCategoria = cat_id and prod_estado=1";
+        $sql = "select * from mas_informacion";
         //preparacion de la sentencia
         $stmt = $this->con->prepare($sql);
         //ejecucion de la sentencia
@@ -23,26 +23,27 @@ class ProductosDAO {
         // retorna datos para el controlador
         return $resultados;
     }
-
-    public function insertar($cod, $nom, $desc, $estado, $precio, $idCat, $usu) {
+    //Terminado
+    public function insertar($nombre, $apellidos, $telefono, $correo, $asunto, $mensaje) {
         //sentencia sql
-        $sql = "INSERT INTO productos (prod_id, prod_codigo, prod_nombre, prod_descripcion, prod_estado, prod_precio, 
-            prod_idCategoria, prod_usuarioActualizacion, prod_fechaActualizacion) VALUES 
-            (NULL, :cod, :nom, :desc, :estado, :precio, :idCat, :usu, :fecha)";
+        //$sql = "INSERT INTO productos (prod_id, prod_codigo, prod_nombre, prod_descripcion, prod_estado, prod_precio, 
+            // prod_idCategoria, prod_usuarioActualizacion, prod_fechaActualizacion) VALUES 
+            // (NULL, :cod, :nom, :desc, :estado, :precio, :idCat, :usu, :fecha)";
+
+        $sql = "INSERT INTO mas_informacion (`nombre`, `apellidos`, `telefono`, `correo`, `asunto`, `mensaje`) 
+                VALUES ('$nombre', '$apellidos', '$telefono', '$correo', '$asunto', '$mensaje');";
        
         //bind parameters
         $sentencia = $this->con->prepare($sql);
         $fechaActual = new DateTime('NOW');
         $strfecha = $fechaActual->format('Y-m-d H:i:s');
         $data = [
-            'cod' => $cod,
-            'nom' => $nom,
-            'desc' => $desc,
-            'estado' => $estado,
-            'precio' => $precio,
-            'idCat' => $idCat,
-            'usu' => $usu,
-            'fecha' => $strfecha
+            'nombre' => $nombre,
+            'apellidos' => $apellidos,
+            'telefono' => $telefono,
+            'correo' => $correo,
+            'asunto' => $asunto,
+            'mensaje' => $mensaje
         ];
         //execute
         $sentencia->execute($data);
@@ -54,18 +55,55 @@ class ProductosDAO {
         return true;
     }
 
-    public function actualizar() {
-        
+    public function actualizar($id, $nombre, $apellidos, $telefono, $correo, $asunto, $mensaje) {
+        //prepare
+        $sql = "UPDATE `mas_informacion` SET `nombre`='$nombre',
+        `apellidos`='$apellidos',`telefono`='$telefono',`correo`='$correo',
+        `asunto`='$asunto',`mensaje`='$mensaje' WHERE id=$id";
+
+        $sentencia = $this->con->prepare($sql);
+        $data = [
+            'id' => $id,
+            'nombre' => $nombre,
+            'apellidos' => $apellidos,
+            'telefono' => $telefono,
+            'correo' => $correo,
+            'asunto' => $asunto,
+            'mensaje' => $mensaje
+        ];
+
+        $sentencia-> execute($data);
+        //retornar resultados, 
+        if ($sentencia->rowCount() <= 0) { // verificar si se inserto 
+            //rowCount permite obtner el numero de filas afectadas
+            return false;
+        }
+        return true;
     }
 
-    public function eliminar() {
-        
+    public function eliminar($id) {
+        $sql = "DELETE FROM 'mas_informacion' WHERE id='$id'";
+
+        $sentencia = $this->con->prepare($sql);
+
+        $data = [
+            'id' => $id
+        ];
+
+        //execute
+        $sentencia->execute($data);
+        //retornar resultados, 
+        if ($sentencia->rowCount() <= 0) { // verificar si se inserto 
+            //rowCount permite obtner el numero de filas afectadas
+            return false;
+        }
+        return true;
     }
 
     public function buscar($parametro) {
           // sql de la sentencia
-        $sql = "SELECT * FROM productos, categoria  where prod_idCategoria = cat_id and prod_estado=1  and 
-		(prod_nombre like :b1 or cat_nombre like :b2)";
+        $sql = "SELECT * FROM 'mas_informacion'  where  
+		(nombre like :b1 or apellidos like :b2 or asunto like:b1 or mensaje like:b2)";
         $stmt = $this->con->prepare($sql);
         // preparar la sentencia
         $conlike = '%' . $parametro . '%';
